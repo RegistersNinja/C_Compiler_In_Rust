a3907f6a0746925cbff144b0d84b3a8788cb254b
Zinoviy <a@a>
2024-08-19 11:22:26 +0300

    fixed parser for stage_1 program, now it works

--- c_compiler/src/compiler/parser.rs
+++ c_compiler/src/compiler/parser.rs
@@ -135,7 +135,7 @@ impl Constant {
     }
 }
 
-fn parse_statement(mut tokenized_list: Vec<(String, String)>) -> Result<Statement, String> {
+fn parse_statement(tokenized_list: &mut Vec<(String, String)>) -> Result<Statement, String> {
     if tokenized_list.is_empty() {
         return Err("Parse statement failed".to_string());
     }
@@ -150,7 +150,7 @@ fn parse_statement(mut tokenized_list: Vec<(String, String)>) -> Result<Statemen
     }
 }
 
-fn parse_expression(mut tokenized_list: Vec<(String, String)>) -> Result<Expression, String> {
+fn parse_expression(tokenized_list: &mut Vec<(String, String)>) -> Result<Expression, String> {
     if tokenized_list.len() < 2 {
         return Err("Parse expression failed".to_string());
     }
@@ -174,37 +174,68 @@ fn parse_expression(mut tokenized_list: Vec<(String, String)>) -> Result<Express
     }
 }
 
-fn parse_declaration(mut tokenized_list: Vec<(String, String)>) -> Result<Declaration, String> {
+fn expect(expect: String, actual: String) -> bool {
+    if expect.eq(&actual) {
+        return true
+    } 
+    return false
+    
+}
+
+fn parse_declaration(tokenized_list: &mut Vec<(String, String)>) -> Result<Declaration, String> {
     if tokenized_list.len() < 3 {
         return Err("Parse declaration failed".to_string());
     }
     let return_type = tokenized_list.remove(0).1;
-    let name = tokenized_list.remove(0).1;
-    let parameters = tokenized_list.remove(0).1;
-    Ok(Declaration {
-        return_type,
-        name,
-        parameters,
-    })
+    match return_type.as_str() {
+        "int" => {
+            let name = tokenized_list.remove(0).1;
+            match name.as_str() {
+                "main" => {
+                    if expect("(".to_string(),tokenized_list.remove(0).1) {
+                        let parameters = tokenized_list.remove(0).1;
+                        if expect(")".to_string(), tokenized_list.remove(0).1) {
+                            Ok(Declaration {
+                                return_type,
+                                name,
+                                parameters,
+                            })
+                        } else {
+                            Err("Parse declaration failed".to_string())
+                        }
+
+                    } else {
+                        Err("Parse declaration failed".to_string())
+                    }
+                },  _ => return Err("Parse declaration failed".to_string())
+            }
+        }, _ => return Err("Parse declaration failed".to_string())
+    }
 }
 
-fn parse_body(tokenized_list: Vec<(String, String)>) -> Result<Body, String> {
+fn parse_body(tokenized_list: &mut Vec<(String, String)>) -> Result<Body, String> {
     let statement = parse_statement(tokenized_list)?;
     Ok(Body { statement })
 }
 
-fn parse_function(tokenized_list: Vec<(String, String)>) -> Result<Function, String> {
-    let declaration = parse_declaration(tokenized_list.clone())?;
-    let body = parse_body(tokenized_list)?;
-    Ok(Function { declaration, body })
+fn parse_function(tokenized_list: &mut Vec<(String, String)>) -> Result<Function, String> {
+    let declaration = parse_declaration(tokenized_list)?;
+    if expect("{".to_string(),tokenized_list.remove(0).1){
+        let body = parse_body(tokenized_list)?;
+        if expect("}".to_string(), tokenized_list.remove(0).1){
+            Ok(Function {declaration, body})
+        }else {return Err("Parse body failed".to_string())}
+    } else {return Err("Parse body failed".to_string())}
+    
+    
 }
 
-fn parse_program(tokenized_list: Vec<(String, String)>) -> Result<Program, String> {
+fn parse_program(tokenized_list: &mut Vec<(String, String)>) -> Result<Program, String> {
     let function = parse_function(tokenized_list)?;
     Ok(Program { program: function })
 }
 
-pub fn parser(tokenized_list: Vec<(String, String)>) -> Result<ASTNode, Box<dyn Error>> {
-    let program = parse_program(tokenized_list)?;
+pub fn parser(mut tokenized_list: Vec<(String, String)>) -> Result<ASTNode, Box<dyn Error>> {
+    let program = parse_program(&mut tokenized_list)?;
     Ok(ASTNode {program: program})
 }
